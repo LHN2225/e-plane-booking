c6e54a1cc6f877049658fe8eb6e8ff0f19a8a9c6
lhnhan <hoangnhan2506@gmail.com>
2023-04-10 11:44:57 +0700

    init

---
+++ ePlaneBooking/src/main/java/com/example/ePlaneBooking/EPlaneBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ePlaneBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPlaneBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPlaneBookingApplication.class, args);
+	}
+
+}
---
+++ ePlaneBooking/src/test/java/com/example/ePlaneBooking/EPlaneBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ePlaneBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPlaneBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
